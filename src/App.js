daffd6b8d531feb4038875bb163598e27a967193
arijitchakma79 <arijitchakma79@gmail.com>
2025-02-14 20:39:53 -0500

    fixed some bugs regarding routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Resume, Projects } from './pages';
 import { Sidebar, Footer } from './components'; // Import Footer
 import './styles/App.css';
